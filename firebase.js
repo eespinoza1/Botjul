1d0484c0a3f8af025a625859166ef648789fbc09
eespinoza1 <co.bordados.riot@gmail.com>
2025-04-25 11:49:53 -0500

    firebase

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
 const admin = require('firebase-admin');
+let serviceAccount;
+
 if (process.env.FIREBASE_CONFIG) {
-  // En Railway (usa variable de entorno)
+  // Railway: carga el JSON desde la variable de entorno
   serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 } else {
-  // En local (usa archivo .json)
-  serviceAccount = require('./vecinojuliobot-firebase-adminsdk-fbsvc-0e4f7eabbc.json');
-}// Cambia esto por la ruta a tu archivo
+  // Local: carga el archivo JSON
+  serviceAccount = require('./vecinojuliobot-firebase-adminsdk-fbsvc-0e4f7eabbc.json'); // Asegúrate que esté en esta ruta
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "tu-proyecto.appspot.com" // Opcional si usas Storage
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
-const bucket = admin.storage().bucket(); // Opcional
-
-module.exports = { db, bucket };
+module.exports = { db };
